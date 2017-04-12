53d64e298e464e4759932f3e3e831440d37eae51
helgeanl <helgelang@gmail.com>
2017-04-12 15:18:24 +0200

    New article

--- Project/Traffic sign classifier/training_nets/inception_model.py
+++ Project/Traffic sign classifier/training_nets/inception_model.py
@@ -1,4 +1,5 @@
-from keras.utils import plot_model
+#from keras.utils import plot_model
+from keras.utils.vis_utils import plot_model
 from keras.applications.inception_v3 import InceptionV3
 model = InceptionV3(weights='imagenet', include_top=True)
 
